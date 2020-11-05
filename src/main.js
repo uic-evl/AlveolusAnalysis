aa9a5cc9ac679d6d5ab5fda351c160180c1ad37f
Andrew Burks <aburks@conversantmedia.com>
2020-11-04 20:45:39 -0600

    swap to use the new test_data combined_images

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
 console.log("in main");
 
-const folder1 = "./test_data/combo/";
-const folder2 = "./test_data/maxed_combo/";
+const folder1 = "./test_data/combined_images/control/";
+const folder2 = "./test_data/combined_images/tys/";
 
-const timesteps = [
-  "001200",
-  "001202",
-  "001203",
-  "001205",
-  "001206",
-  "001207",
-  "001208",
-  "001209",
-  "001210",
-  "001212",
-];
+d3.select("#top-image").attr(
+  "src",
+  `${folder1}${"1".toString().padStart(6, "0")}.png`
+);
+d3.select("#bot-image").attr(
+  "src",
+  `${folder2}${"1".toString().padStart(6, "0")}.png`
+);
 
 d3.select("#time-slider").on("input", ({ target }) => {
   const { value } = target;
 
-  d3.select("#top-image").attr("src", `${folder1}${timesteps[value]}.png`);
-  d3.select("#bot-image").attr("src", `${folder2}${timesteps[value]}.png`);
+  d3.select("#top-image").attr(
+    "src",
+    `${folder1}${value.toString().padStart(6, "0")}.png`
+  );
+  d3.select("#bot-image").attr(
+    "src",
+    `${folder2}${value.toString().padStart(6, "0")}.png`
+  );
 });
