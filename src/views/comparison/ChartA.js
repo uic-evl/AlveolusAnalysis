0ab9b712aca16a57065560c5574bf9d4f260e58c
Yushen Dong <dyushen@umich.edu>
2020-11-09 03:02:17 -0600

    testtest

--- src/views/comparison/ChartA.js
+++ src/views/comparison/ChartA.js
@@ -4,7 +4,9 @@ export class ChartA {
 
   constructor({ container }) {
     this.container = container;
-
+    //create the svg
+    this.width = this.style('width'); this.height = this.width / 2;
+    this.margin = {top: 20, right: 75, bottom: 45, left: 50};
     // ... do stuff here
 
     console.log("ChartA", this);
