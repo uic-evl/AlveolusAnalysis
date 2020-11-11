4c87c2d335629f8c0ca70fcb3e3d0646ba172761
alexp205 <politowicz205@gmail.com>
2020-11-11 11:44:52 -0600

    added feature extraction scripts, updated processing scripts, cleaned Python code

--- python/dens_analyzer.py
+++ python/dens_analyzer.py
@@ -4,33 +4,7 @@ import os
 import re
 import matplotlib.pyplot as plt
 
-# consistency/counting stuff
-    #last_num = -1
-    #total_lost = 0
-
-    #    code_str = re.split("\.png", fname)[0]
-    #    if -1 == last_num:
-    #        try:
-    #            last_num = int(code_str)
-    #        except:
-    #            print("problem")
-    #            pass
-    #    else:
-    #        try:
-    #            #print(last_num)
-    #            #print(int(code_str))
-    #            #print(int(code_str) - (last_num + 1))
-    #            #input("wait")
-    #            total_lost += int(code_str) - (last_num + 1)
-    #            last_num = int(code_str)
-    #        except:
-    #            print("problem")
-    #            pass
-
-    #    #if 5 == idx:
-    #    #    break
-
-    #print(total_lost)
+# generates a heatmap of image densities over the entire timeline
 
 data_dir = "./hksa-tys_2-032/combo/"
 
@@ -42,17 +16,8 @@ def main():
     total_count = 0
 
     for idx, fname in enumerate(os.listdir(data_dir)):
-        #print("fname:")
-        #print(fname)
         img = cv2.imread(data_dir + fname)
-        #print("img:")
-        #print(img)
-        #print(img.shape)
-        #cv2.imshow("a", img)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
 
-        #new_img = np.asarray([ch2_img, phold_img, ch4_img]) # for ref
         # do regular counting
         image_counts[np.sum(img, axis=2) > 0] += 1
 
@@ -64,17 +29,10 @@ def main():
 
         total_count += 1
 
-        #if idx > 10:
-        #    break
-
     # calc densities
     image_dens = image_counts / total_count
     ch2_dens = ch2_counts / total_count
     ch4_dens = ch4_counts / total_count
-
-    #print(image_dens)
-    #print(ch2_dens)
-    #print(ch4_dens)
 
     # plot heatmaps
     # - image
