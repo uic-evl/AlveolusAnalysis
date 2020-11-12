edb7ccba2d022513739483fd9f06f83cc149eb20
Andrew Burks <andrewtburks@gmail.com>
2020-11-12 04:31:52 -0600

    dont auto-add experiments to interface

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
 import { ComparisonController } from "./controllers/ComparisonController.js";
 
 const comparison = new ComparisonController();
-
-comparison.setTopExperiment({ name: "control" });
-comparison.setBotExperiment({ name: "tys" });
