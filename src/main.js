1e01c18a09af5f5d0a8bf31ff6e6238ee7c53217
Andrew Burks <andrewtburks@gmail.com>
2020-11-29 04:20:46 -0600

    add tippy/popper to vis and add tooltips on time buttons

--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ const comparison = new ComparisonController();
 comparison.setTopExperiment({ name: "control" });
 comparison.setBotExperiment({ name: "tys" });
 
-// const flowView = new FlowView({
-//   flowContainer: d3.select("#flow"),
+// set up basic ui tooltips
+tippy("#step-back", { content: "Slow Down", animation: "scale" });
+tippy("#play-button", { content: "Play/Pause", animation: "scale" });
+tippy("#step-forward", { content: "Step Forward", animation: "scale" });
 
-//   experimentsContainer: d3.select("#animals"),
-//   topContainer: d3.select("#top-experiment"),
-//   botContainer: d3.select("#bot-experiment"),
-//   compareContainer: d3.select("#compare"),
-// });
+tippy("#slow-down", { content: "Slow Down", animation: "scale" });
+tippy("#speed-up", { content: "Speed Up", animation: "scale" });
